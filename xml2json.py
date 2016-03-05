401e73635bf079cea896f66a2a73cff7f6cd3537
mraburn <mraburn@gmail.com>
2016-03-05 08:41:15 -0600

    Update xml2json.py
    FInding new lines

--- xml2json.py
+++ xml2json.py
@@ -234,7 +234,7 @@ def main():
     if options.strip_nl:
         input = input.replace('\n', '').replace('\n','')
     if (options.type == "xml2json"):
-        out = xml2json(input, options, strip_ns, strip)
+        out = xml2json(input, options, strip)
     else:
         out = json2xml(input)
 
